40a3f6e6d6d77ae014a1ea42c72ce56793046bc0
Manoj528447 <120321023+Manoj528447@users.noreply.github.com>
2026-05-13 17:30:53 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
-import streamlit as st
-st.title("Test App")
+import gradio as gr
+
+def greet(name):
+    return f"Hello {name}"
+
+demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+demo.launch(server_name="0.0.0.0", server_port=7860)
